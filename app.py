b5402ec8f843127db5031c65f3abfa7386f2d1e1
mj7910 <manoj.subedi54@gmail.com>
2025-04-27 20:27:56 -0500

    sds

--- app.py
+++ app.py
@@ -8,16 +8,16 @@ warnings.filterwarnings('ignore')
 st.set_page_config(page_title="Superstore!!!", page_icon=":bar_chart:",layout="wide")
 
 st.title(" :bar_chart: Sample SuperStore EDA")
-st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
+st.markdown('<style>div.block-container{padding-top:2rem;}</style>',unsafe_allow_html=True)
 
 fl = st.file_uploader(":file_folder: Upload a file",type=(["csv","txt","xlsx","xls"]))
 if fl is not None:
     filename = fl.name
     st.write(filename)
-    df = pd.read_csv(filename, encoding = "ISO-8859-1")
+    df = pd.read_csv(filename)
 else:
-    os.chdir(r"C:\Users\AEPAC\Desktop\Streamlit")
-    df = pd.read_csv("data\Data.csv", encoding = "ISO-8859-1")
+    os.chdir(r"C:\Users\msharma7\Documents\GitHub\Project-01")
+    df = pd.read_excel(r"C:\Users\msharma7\Documents\GitHub\Project-01\data\Data.xls")
 
 col1, col2 = st.columns((2))
 df["Order Date"] = pd.to_datetime(df["Order Date"])
