64936458dd46525478140a0734296672356d92d2
mj7910 <manoj.subedi54@gmail.com>
2025-04-27 20:12:59 -0500

    data upload

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ if fl is not None:
     df = pd.read_csv(filename, encoding = "ISO-8859-1")
 else:
     os.chdir(r"C:\Users\AEPAC\Desktop\Streamlit")
-    df = pd.read_csv("Superstore.csv", encoding = "ISO-8859-1")
+    df = pd.read_csv("data\Data.csv", encoding = "ISO-8859-1")
 
 col1, col2 = st.columns((2))
 df["Order Date"] = pd.to_datetime(df["Order Date"])
